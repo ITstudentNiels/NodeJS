f4c3c685f9828987a6a949b9eefd52a9e9624571
ITstudentNiels <460499@student.fontys.nl>
2021-09-23 11:57:09 +0000

    Res send message changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World, Dit werkt. Toch?');
 });
 
 app.listen(5000, () => console.log('Running on port 5000'));
