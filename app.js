bef5ddcefa7419983d24cbd2b5b6c98fa4f7d806
ITstudentNiels <460499@student.fontys.nl>
2021-09-30 07:18:13 +0000

    Erik

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Erik!');
 });
 
 app.listen(5000, () => console.log('Running on port 5000'));
